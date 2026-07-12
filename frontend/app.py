9038b1b3d3493ce5aa264951adca68261f2af815
minhdavideragagni <100780089+minhdavideragagni@users.noreply.github.com>
2026-07-12 20:28:36 +0200

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -15,7 +15,7 @@ import streamlit as st
 
 APP_NAME = "CQ-Genesis"
 APP_SUBTITLE = (
-    "Eliciting Competency Questions from Structured Data and User Stories"
+    "CQ-Genesis: LLM-Assisted and Human-Guided Competency Question Generation from Structured Data and User Stories"
 )
 
 BACKEND_URL = os.getenv(
